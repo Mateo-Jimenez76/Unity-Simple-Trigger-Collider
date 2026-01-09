1f0ea57337ed572211a59e8b7e66d6791f4ac5d7
Mateo-Jimenez76 <185623516+Mateo-Jimenez76@users.noreply.github.com>
2026-01-09 07:22:24 -0800

    chore(CustomSettings): Add data and documentation

--- Editor/CustomSettings.cs
+++ Editor/CustomSettings.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 using UnityEditor;
 public class CustomSettings : ScriptableObject
 {
+    // Path to the asset that will hold the settings information
+    // "Asset/Editor" restricts your ability to use these settings at runtime
+    // So "Assets/Resources" is a better location if runtime access is needed
     public const string settingsPath = "Assets/Resources/CustomSettings.asset";
 
-    [SerializeField] private bool enableDebugLogs;
+    // --- Settings for your package ---
+    [SerializeField] private bool debugLogs;
 
+    [SerializeField] private ColliderType defaultColliderType;
+
+    [SerializeField] private Collider2DType defaultCollider2DType;
 
     internal static CustomSettings GetOrCreateSettings()
     {
@@ -13,8 +20,15 @@ public class CustomSettings : ScriptableObject
 
         if (settings == null)
         {
+            //Create an instance of the settings object
             settings = CreateInstance<CustomSettings>();
-            settings.enableDebugLogs = true;
+
+            //Set default values for settings
+            settings.debugLogs = true;
+            settings.defaultColliderType = ColliderType.Box;
+            settings.defaultCollider2DType = Collider2DType.Box;
+
+            //Save the settings object as an asset
             AssetDatabase.CreateAsset(settings, settingsPath);
             AssetDatabase.SaveAssets();
         }
@@ -24,5 +38,21 @@ public class CustomSettings : ScriptableObject
     internal static SerializedObject GetSerializedSettings()
     {
         return new SerializedObject(GetOrCreateSettings());
+    }
+
+    private enum ColliderType
+    {
+        Box,
+        Sphere,
+        Capsule,
+        Mesh
+    }
+
+    private enum Collider2DType
+    {
+        Box,
+        Circle,
+        Polygon,
+        Edge
     }
 }
