479c7251f282aa9893de47768e4548965d0ef40d
Mateo-Jimenez76 <185623516+Mateo-Jimenez76@users.noreply.github.com>
2026-02-14 23:42:28 -0800

    Added namespaces and made prefabs for demo scene

--- Samples/EnemyController.cs
+++ Samples/EnemyController.cs
@@ -1,36 +1,39 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class EnemyController : MonoBehaviour
+namespace SimpleTriggerColliders.Demo
 {
-    [SerializeField] float speed = 5f;
-    [SerializeField] GameObject agroIndicator; // Optional: A visual indicator for when the enemy is agro
-
-    private bool isAgro = false;
-    private Rigidbody2D rb;
-    private Transform target;
-    private void Awake()
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class EnemyController : MonoBehaviour
     {
-        agroIndicator?.SetActive(false); // Ensure the agro indicator is off at the start
+        [SerializeField] float speed = 5f;
+        [SerializeField] GameObject agroIndicator; // Optional: A visual indicator for when the enemy is agro
 
-        rb = GetComponent<Rigidbody2D>();  
-
-        target = FindAnyObjectByType<PlayerMovement>().transform;
-    }
-
-    public void BecomeAgro()
-    {
-        isAgro = true;
-    }
-
-    private void Update()
-    {
-        if (isAgro)
+        private bool isAgro = false;
+        private Rigidbody2D rb;
+        private Transform target;
+        private void Awake()
         {
-            agroIndicator?.SetActive(true);
-            // Move towards the target
-            Vector2 direction = (target.position - transform.position).normalized;
-            rb.linearVelocity = direction * speed;
+            agroIndicator?.SetActive(false); // Ensure the agro indicator is off at the start
+
+            rb = GetComponent<Rigidbody2D>();
+
+            target = FindAnyObjectByType<PlayerMovement>().transform;
+        }
+
+        public void BecomeAgro()
+        {
+            isAgro = true;
+        }
+
+        private void Update()
+        {
+            if (isAgro)
+            {
+                agroIndicator?.SetActive(true);
+                // Move towards the target
+                Vector2 direction = (target.position - transform.position).normalized;
+                rb.linearVelocity = direction * speed;
+            }
         }
     }
 }
--- Samples/PlayerMovement.cs
+++ Samples/PlayerMovement.cs
@@ -1,39 +1,41 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-//Required For Applying forces
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovement : MonoBehaviour
+namespace SimpleTriggerColliders.Demo
 {
-    [SerializeField] private float _moveSpeed = 5f;
-
-    private float _horizontalInput;
-    private float _verticalInput;
-    private Rigidbody2D _rb;
-    private InputAction _move;
-    private void Awake()
+    //Required For Applying forces
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovement : MonoBehaviour
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _rb.interpolation = RigidbodyInterpolation2D.Interpolate;
-        _rb.freezeRotation = true;
+        [SerializeField] private float _moveSpeed = 5f;
 
-        _move = InputSystem.actions["Move"];
+        private float _horizontalInput;
+        private float _verticalInput;
+        private Rigidbody2D _rb;
+        private InputAction _move;
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+            _rb.freezeRotation = true;
 
-    }
+            _move = InputSystem.actions["Move"];
 
-    // Update is called once per frame
-    void Update()
-    {
-        _horizontalInput = _move.ReadValue<Vector2>().x;
-        _verticalInput = _move.ReadValue<Vector2>().y;
-    }
-    private void FixedUpdate()
-    {
-        _rb.linearVelocity = new Vector2(_horizontalInput, _verticalInput) * _moveSpeed;
-    }
+        }
 
-    public void SetSpeed(float speed)
-    {
-        _moveSpeed = speed;
+        // Update is called once per frame
+        void Update()
+        {
+            _horizontalInput = _move.ReadValue<Vector2>().x;
+            _verticalInput = _move.ReadValue<Vector2>().y;
+        }
+        private void FixedUpdate()
+        {
+            _rb.linearVelocity = new Vector2(_horizontalInput, _verticalInput) * _moveSpeed;
+        }
+
+        public void SetSpeed(float speed)
+        {
+            _moveSpeed = speed;
+        }
     }
 }
