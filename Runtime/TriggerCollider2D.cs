66099123ca328a32d156c750b8478ae394e5a0b4
Mateo Jimenez <tidy-scorer-stem@duck.com>
2026-07-05 19:41:56 -0700

    Fix(Trigger Colliders): Unable to find package settings in Resources folder

--- Runtime/TriggerCollider2D.cs
+++ Runtime/TriggerCollider2D.cs
@@ -54,7 +54,7 @@ namespace SimpleTriggerCollider.Runtime
             }
 
             //Load Package Settings
-            var settings = Resources.Load<CustomSettings>("SimpleTriggerColliderSettings"); 
+            var settings = Resources.Load<CustomSettings>("Simple Trigger Collider/SimpleTriggerColliderSettings"); 
 
             //Check for what kind of Collider2D to create
             switch (settings.GetDefaultCollider2DType())
